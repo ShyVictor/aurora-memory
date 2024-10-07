5b73d849c642a26803d9fd3bf48221c5f4d0fbcb
ShyVictor <leefaker56@gmail.com>
2024-10-07 04:42:47 -0300

    setup: initialize the default project structure

---
+++ include/aurora.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+typedef unsigned int int32;
+typedef unsigned short int int16;
+typedef unsigned char int8;
+
+int main(int, char**);
+void server_container(void);
+
---
+++ src/aurora.c
@@ -0,0 +1,11 @@
+#include "../include/aurora.h"
+
+bool server_container;
+
+int main(int argc, char* argv[]){
+    server_container = true;
+    while (server_container)
+
+    printf("Aurora in-memory database initialized\n");
+    return 0;
+}
